fbe27f2fdb01beec9782463875f8df2a301fceac
erkle64 <erkle64@gmail.com>
2024-06-19 02:36:14 +1200

    Added config option to set crafting time decrease percentage.

--- RapidPocketCrafting/Plugin.cs
+++ RapidPocketCrafting/Plugin.cs
@@ -11,18 +11,39 @@ namespace RapidPocketCrafting
             MODNAME = "RapidPocketCrafting",
             AUTHOR = "erkle64",
             GUID = AUTHOR + "." + MODNAME,
-            VERSION = "0.1.0";
+            VERSION = "0.2.0";
 
         public static LogSource log;
+
+        public static TypedConfigEntry<float> configCraftingTimeDecreasePercentage;
 
         public Plugin()
         {
             log = new LogSource(MODNAME);
+
+            new Config(GUID)
+                .Group("General")
+                    .Entry(
+                        out configCraftingTimeDecreasePercentage,
+                        "Crafting time decrease percentage",
+                        90.0f,
+                        "Percentage of crafting time to remove.",
+                        "Maximum: 90",
+                        "Negative numbers will increase crafting time")
+                .EndGroup()
+                .Load()
+                .Save();
         }
 
         public override void Load(Mod mod)
         {
             log.Log($"Loading {MODNAME}");
+        }
+
+        public static long GetCraftingTimeDecrease()
+        {
+            var value = (long)(configCraftingTimeDecreasePercentage.Get() * 10000.0f);
+            return value > 900000L ? 900000L : value;
         }
 
         [HarmonyPatch]
@@ -32,14 +53,14 @@ namespace RapidPocketCrafting
             [HarmonyPostfix]
             public static void CharacterManagerInit()
             {
-                CharacterManager.characterManager_setCharacterCraftingSpeedDecrementPercentage(900000L);
+                CharacterManager.characterManager_setCharacterCraftingSpeedDecrementPercentage(GetCraftingTimeDecrease());
             }
 
             [HarmonyPatch(typeof(CharacterManager), nameof(CharacterManager.increasePlayerCharacterCraftingSpeedByResearch))]
             [HarmonyPostfix]
             public static void CharacterManagerIncreasePlayerCharacterCraftingSpeedByResearch()
             {
-                CharacterManager.characterManager_setCharacterCraftingSpeedDecrementPercentage(900000L);
+                CharacterManager.characterManager_setCharacterCraftingSpeedDecrementPercentage(GetCraftingTimeDecrease());
             }
         }
     }
